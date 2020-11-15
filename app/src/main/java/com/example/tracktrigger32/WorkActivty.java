8145482a65bca1d9ce72c0acedbdc9cb9240514f
bot977 <ypavan2802@gmail.com>
2020-11-15 15:32:50 +0530

    Creating activities for Household Pages and Work Pages.

---
+++ app/src/main/java/com/example/tracktrigger32/WorkActivty.java
@@ -0,0 +1,14 @@
+package com.example.tracktrigger32;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class WorkActivty extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_work_activty);
+    }
+}

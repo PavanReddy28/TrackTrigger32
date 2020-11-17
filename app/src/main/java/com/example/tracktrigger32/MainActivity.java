60540d78e1b3fb40b00bb370d9b40d1b0f7bb8ad
bot977 <ypavan2802@gmail.com>
2020-11-17 20:18:07 +0530

    Nav drawer.

--- app/src/main/java/com/example/tracktrigger32/MainActivity.java
+++ app/src/main/java/com/example/tracktrigger32/MainActivity.java
@@ -3,6 +3,7 @@ package com.example.tracktrigger32;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -12,4 +13,39 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
     }
+
+    public void ClickMenu(View view) {
+        //open drawer
+        openDrawer(drawerLayout);
+    }
+
+    public void ClickProfile(View view){
+        closeDrawer(drawerLayout);
+    }
+
+    public void ClickHome(View view){
+        closeDrawer(drawerLayout);
+    }
+
+    public void ClickHouse(View view){
+        redirectActivity(this, HouseholdActivity.class);
+    }
+
+    public void ClickWork(View view){
+        redirectActivity(this, WorkActivity.class);
+    }
+
+    public void ClickNotes(View view){
+        redirectActivity(this, NotesActivity.class);
+    }
+
+    public void ClickSettings(View view){
+        redirectActivity(this, SettingsActivity.class);
+    }
+
+    //Navigation Functionality ends
+
+
+
+
 }

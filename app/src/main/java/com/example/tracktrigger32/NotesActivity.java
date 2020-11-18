bc530a89959953e48b692da2e2cc61bb199e31f1
bot977 <ypavan2802@gmail.com>
2020-11-18 22:50:31 +0530

    Firebase Authentication added.

--- app/src/main/java/com/example/tracktrigger32/NotesActivity.java
+++ app/src/main/java/com/example/tracktrigger32/NotesActivity.java
@@ -9,7 +9,7 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 
-public class NotesActivity extends AppCompatActivity {
+public class NotesActivity extends AppCompatActivity{
 
     DrawerLayout drawerLayout;
 
---
+++ app/src/main/java/com/example/tracktrigger32/auth/FirebaseUI.java
@@ -0,0 +1,19 @@
+package com.example.tracktrigger32.auth;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.example.tracktrigger32.R;
+
+public class FirebaseUI extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_firebase_u_i);
+
+    }
+
+
+}

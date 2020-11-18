974973c2a341d5e23207c3769e41db54bbfd97b9
vineet <vineetvenkatesh9@gmail.com>
2020-11-18 12:55:05 +0530

    Commit by Vineet..

---
+++ app/src/main/java/com/example/tracktrigger32/ReminderActivity.java
@@ -0,0 +1,37 @@
+package com.example.tracktrigger32;
+
+import java.util.Date;
+
+//@Entity(tableName = "reminder")
+public class ReminderActivity {
+    //@PrimaryKey(autoGenerate = true)
+    //@NonNull
+    public int id;
+
+    String message;
+    Date remindDate;
+
+    public String getMessage() {
+        return message;
+    }
+
+    public Date getRemindDate() {
+        return remindDate;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setMessage(String message) {
+        this.message = message;
+    }
+
+    public void setRemindDate(Date remindDate) {
+        this.remindDate = remindDate;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+}
---
+++ app/src/main/java/com/example/tracktrigger32/ReminderAdapter.java
@@ -0,0 +1,56 @@
+package com.example.tracktrigger32;
+
+
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+import java.util.List;
+
+public class ReminderAdapter extends RecyclerView.Adapter<ReminderAdapter.MyViewHolder>{
+
+    private List<ReminderActivity> allReminders;  //arraylist
+    private TextView message,time;
+
+    public ReminderAdapter(List<ReminderActivity> allReminders) {   //context
+        this.allReminders = allReminders;
+    }
+
+    @NonNull
+    @Override
+    public MyViewHolder onCreateViewHolder(@NonNull ViewGroup viewGroup, int i) {  //adaptereminders.viewholder
+
+        View view = LayoutInflater.from(viewGroup.getContext()).inflate(R.layout.schedule_item,viewGroup,false);
+        return new MyViewHolder(view);
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull MyViewHolder myViewHolder, int i) {
+
+        ReminderActivity reminders = allReminders.get(i); // myViewHolder.itemView.settag(allreminders.get(i))
+        if(!reminders.getMessage().equals(""))
+            message.setText(reminders.getMessage());
+        else
+            message.setHint("No Message");
+        time.setText(reminders.getRemindDate().toString());
+
+    }
+
+    @Override
+    public int getItemCount() {
+        return allReminders.size();
+    }
+
+    public class MyViewHolder extends RecyclerView.ViewHolder{
+
+        public MyViewHolder(@NonNull View itemView) {
+            super(itemView);
+            message = itemView.findViewById(R.id.textView1);
+            time = itemView.findViewById(R.id.textView2);
+        }
+    }
+
+}

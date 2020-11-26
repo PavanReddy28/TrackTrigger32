e85f4af29b41ebf4c65a5036da55fbef0e326361
ishaan <ishaankhushu123@gmail.com>
2020-11-26 22:10:37 +0530

    Notes Activity by Ishaan

--- app/src/main/java/com/example/tracktrigger32/NotesActivity.java
+++ app/src/main/java/com/example/tracktrigger32/NotesActivity.java
@@ -1,33 +1,91 @@
 package com.example.tracktrigger32;
 
-import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.view.GravityCompat;
 import androidx.drawerlayout.widget.DrawerLayout;
 
 import android.app.Activity;
+import android.app.AlertDialog;
+import android.content.DialogInterface;
 import android.content.Intent;
 import android.os.Bundle;
-import android.view.MenuItem;
 import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
 
-import com.google.android.material.navigation.NavigationView;
-import com.google.firebase.auth.FirebaseAuth;
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
 
-public class NotesActivity extends AppCompatActivity implements NavigationView.OnNavigationItemSelectedListener{
+import java.util.ArrayList;
+
+public class NotesActivity extends AppCompatActivity {
 
     DrawerLayout drawerLayout;
+    static ArrayList<String> notes = new ArrayList<>() ;
+    static ArrayAdapter arrayAdapter ;
+private FloatingActionButton add ;
+
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_notes);
+        add = findViewById(R.id.ibAdd) ;
         drawerLayout = findViewById(R.id.drawer_layout);
-        NavigationView navigationView = (NavigationView) findViewById(R.id.navigation_view);
-        navigationView.setNavigationItemSelectedListener(this);
-        navigationView.setCheckedItem(R.id.drawer_notes);
+        ListView listView = (ListView) findViewById(R.id.listView) ;
+        notes.add("Example note") ;
+        add.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(getApplicationContext(),NoteEditorActivity.class) ;
+                startActivity(intent) ;
+
+            }
+        }) ;
+
+        arrayAdapter = new ArrayAdapter(this, android.R.layout.simple_list_item_1, notes) ;
+        listView.setAdapter(arrayAdapter);
+        listView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> adapterView, View view, int i, long l) {
+                Intent intent = new Intent(getApplicationContext(),NoteEditorActivity.class) ;
+                intent.putExtra("noteId" , i ) ;
+                startActivity(intent) ;
+
+
+            }
+        } );
+        listView.setOnItemLongClickListener(new AdapterView.OnItemLongClickListener() {
+            @Override
+            public boolean onItemLongClick(AdapterView<?> adapterView, View view, int i, long l) {
+                int itemToDelete = i ;
+
+                new AlertDialog.Builder(NotesActivity.this)
+                .setIcon(android.R.drawable.ic_dialog_alert)
+                        .setTitle("Are you sure ?")
+                        .setMessage("Do you want to delete this note ? ")
+                        .setPositiveButton("Yes", new DialogInterface.OnClickListener() {
+                            @Override
+                            public void onClick(DialogInterface dialogInterface, int i) {
+                                notes.remove(itemToDelete) ;
+                                arrayAdapter.notifyDataSetChanged();
+
+                            }
+                        }
+                        )
+                 .setNegativeButton("No" , null)
+                 .show() ;
+                return true;
+            }
+        });
     }
 
+    @Override
+    protected void onPause() {
+        super.onPause();
+        closeDrawer(drawerLayout);
+    }
 
     //Navigation Drawer Functionality
 
@@ -41,12 +99,6 @@ public class NotesActivity extends AppCompatActivity implements NavigationView.O
 
         //Start activity
         activity.startActivity(intent);
-    }
-
-    @Override
-    protected void onPause() {
-        super.onPause();
-        closeDrawer(drawerLayout);
     }
 
     public static void openDrawer(DrawerLayout drawerLayout) {
@@ -66,33 +118,30 @@ public class NotesActivity extends AppCompatActivity implements NavigationView.O
         openDrawer(drawerLayout);
     }
 
-    @Override
-    public boolean onNavigationItemSelected(@NonNull MenuItem item) {
-        switch(item.getItemId()) {
-            case R.id.drawer_home:
-                redirectActivity(this, MainActivity.class);
-                break;
-            case R.id.drawer_house:
-                redirectActivity(this, HouseholdActivity.class);
-                break;
-            case R.id.drawer_work:
-                redirectActivity(this, WorkActivity.class);
-                break;
-            case R.id.drawer_notes:
-                break;
-            case R.id.drawer_settings:
-                redirectActivity(this, SettingsActivity.class);
-                break;
-            case R.id.drawer_logout:
-                FirebaseAuth.getInstance().signOut();
-                redirectActivity(this, MainActivity.class);
-                finish();
-                break;
-        }
-
-        drawerLayout.closeDrawers();
-        return true;
+    public void ClickProfile(View view){
+        closeDrawer(drawerLayout);
     }
+
+    public void ClickHome(View view){
+        redirectActivity(this,MainActivity.class);
+    }
+
+    public void ClickHouse(View view){
+        redirectActivity(this, HouseholdActivity.class);
+    }
+
+    public void ClickWork(View view){
+        redirectActivity(this, WorkActivity.class);
+    }
+
+    public void ClickNotes(View view){
+        closeDrawer(drawerLayout);
+    }
+
+    public void ClickSettings(View view){
+        redirectActivity(this, SettingsActivity.class);
+    }
+
 
     //Navigation Functionality ends
 
